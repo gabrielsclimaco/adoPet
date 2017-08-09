e7305dc2ab3db77a956805d531d881b311390fc6
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-08-09 20:23:56 -0300

    ➕  Add Vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
